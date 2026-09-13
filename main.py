2262a7f0060eba7822cbec5731ccd127f9beb860
munworx <munworx@gmail.com>
2026-09-13 18:18:48 +1000

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://github.io", "http://github.io"],
+    allow_origin_regex=".*",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
